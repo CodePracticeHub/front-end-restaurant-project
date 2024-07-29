9b881f113dcb2a0abea03c47f5a2e39e0faf9a6a
omar <abroumomar1@gmail.com>
2024-07-29 23:05:55 +0200

    Guest Reservation page is ready - NOTE: still needs few logics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HeaderNav } from "./react-components/header/header.tsx";
 import Footer from "./react-components/footer/footer.tsx";
 import Homepage from "./react-components/homepage/homepage.tsx";
 import { Routes, Route, useLocation } from "react-router-dom";
-import ReservationRev from "./react-components/ReservationReview/ReservationRev.tsx";
+// import ReservationRev from "./react-components/ReservationReview/ReservationRev.tsx";
 import Login from "./react-components/SignUp/Registration/Login.tsx";
 import Signup from "./react-components/SignUp/Registration/Signup.tsx";
 import BottomPage from "./react-components/Top-and-Bottom-comp/BottomPage.tsx";
@@ -32,7 +32,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/Sign-in" element={<Login />}></Route>
         <Route path="/Sign-up" element={<Signup />}></Route>
-        <Route path="/reservation-details" element={<ReservationRev />}></Route>
+        {/* <Route path="/reservation-details" element={<ReservationRev />}></Route> On the work */}
 
         <Route path="/Guest-reservation" element={<GuestReservation/>}></Route>  
       </Routes>
