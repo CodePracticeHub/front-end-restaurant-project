d653fc6f72ee03ba055a9bb0b4fc7e593177ba8c
omar <abroumomar1@gmail.com>
2024-08-12 03:42:09 +0200

    user dashboard

---
+++ src/hooks/UpdateUserHook.ts
@@ -0,0 +1,8 @@
+
+
+export default function useUpdateUserHook() {
+
+    // post new user profile changes 
+
+
+}
---
+++ src/react-components/User_Profile/Profile_header/YourAccount.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './YourAccountStyle.css'
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
+
+export default function YourAccount() {
+
+      const navigate = useNavigate()
+
+  return (  
+        <div className='Your-Account-Container'>
+            <div className='your_acc_Sub_Container'>
+                  <FaRegArrowAltCircleLeft onClick={()=> navigate(-1)} className='arrow' size={25}/> 
+                  <h2>  Your Account </h2>
+            </div>      
+        </div>
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
