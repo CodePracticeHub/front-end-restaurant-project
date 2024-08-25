11a3d5945b34377f791ecf6aa020fdc22505a271
omar <abroumomar1@gmail.com>
2024-08-26 01:00:42 +0200

    dashboard route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { lazy, useEffect, Suspense } from "react";
 import { HeaderNav } from "./react-components/header/header.tsx";
 import Footer from "./react-components/footer/footer.tsx";
 import Homepage from "./react-components/homepage/homepage.tsx";
@@ -10,6 +10,13 @@ import Signup from "./react-components/SignUp/Registration/Signup.tsx";
 import BottomPage from "./react-components/Top-and-Bottom-comp/BottomPage.tsx";
 import TopPage from "./react-components/Top-and-Bottom-comp/TopPage.tsx";
 import GuestReservation from "./react-components/Guest-Reserv/GuestReservation.jsx";
+import { SpinnerLoading } from "./react-components/utils/SpinnerLoading.tsx";
+
+
+const UserDashboard = lazy(()=> import("./react-components/User_Profile/User-dash/UserDashboard.tsx"))
+const AccouInfo = lazy(()=> import("./react-components/User_Profile/Accountinformation/AccountInfo.tsx"))
+
+
 
 function ReloadOnRouteChange() {
   const location = useLocation();
@@ -28,15 +35,23 @@ function App() {
   return (
     <>
       {changeH_F(<HeaderNav />, <TopPage />)}
-      <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/Sign-in" element={<Login />}></Route>
-        <Route path="/Sign-up" element={<Signup />}></Route>
+      <Suspense fallback={<SpinnerLoading/>}>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/Sign-in" element={<Login />}></Route>
+          <Route path="/Sign-up" element={<Signup />}></Route>
         {/* <Route path="/reservation-details" element={<ReservationRev />}></Route> On the work */}
 
-        <Route path="/Guest-reservation" element={<GuestReservation/>}></Route>  
-      </Routes>
+          <Route path="/Guest-reservation" element={<GuestReservation/>}></Route> 
+        
+          <Route path="/dashboard" element={<UserDashboard/>}></Route>  
+          <Route path="/dashboard/account-infos" element={<AccouInfo/>}></Route>    
+
+        </Routes>
+      </Suspense>
+
       {changeH_F(<Footer />, <BottomPage />)}
+
     </>
   );
 }
